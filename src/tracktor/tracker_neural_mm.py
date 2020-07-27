7461daef2693d5be535464a3c94ad2db5746a178
Liu Tianji <tefantasy@gmail.com>
2020-07-27 08:56:44 +0100

    minor fix

--- src/tracktor/tracker_neural_mm.py
+++ src/tracktor/tracker_neural_mm.py
@@ -115,14 +115,16 @@ class TrackerNeuralMM(object):
         for i in range(len(self.tracks) - 1, -1, -1):
             t = self.tracks[i]
             t.score = scores[i]
-            if scores[i] <= self.regression_person_thresh:
+            if scores[i] <= 0.05:
+                self.tracks_to_inactive([t])
+            elif scores[i] <= self.regression_person_thresh:
                 # self.tracks_to_inactive([t])
                 t.low_score_length += 1
                 if t.low_score_length > self.low_score_patience:
                     self.tracks_to_inactive([t])
             else:
                 t.low_score_length = 0
-                
+
                 s.append(scores[i])
                 if t.init_motion:
                     t.pos = regress_pos[i].view(1, -1)
