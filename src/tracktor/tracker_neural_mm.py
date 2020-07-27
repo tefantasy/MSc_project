a91c66d9e6f27ef0367521825e216bc7b1665d79
Liu Tianji <tefantasy@gmail.com>
2020-07-27 10:23:36 +0100

    rollback

--- src/tracktor/tracker_neural_mm.py
+++ src/tracktor/tracker_neural_mm.py
@@ -53,7 +53,6 @@ class TrackerNeuralMM(object):
         self.reid_iou_threshold = tracker_cfg['reid_iou_threshold']
         self.do_align = tracker_cfg['do_align']
         self.motion_model_cfg = tracker_cfg['motion_model']
-        self.low_score_patience = 6
 
         self.warp_mode = eval(tracker_cfg['warp_mode'])
         self.number_of_iterations = tracker_cfg['number_of_iterations']
@@ -116,13 +115,8 @@ class TrackerNeuralMM(object):
             t = self.tracks[i]
             t.score = scores[i]
             if scores[i] <= self.regression_person_thresh:
-                # self.tracks_to_inactive([t])
-                t.low_score_length += 1
-                if t.low_score_length > self.low_score_patience:
-                    self.tracks_to_inactive([t])
+                self.tracks_to_inactive([t])
             else:
-                t.low_score_length = 0
-                
                 s.append(scores[i])
                 if t.init_motion:
                     t.pos = regress_pos[i].view(1, -1)
@@ -566,7 +560,6 @@ class Track(object):
         self.gt_id = None
 
         self.vis = -2.0
-        self.low_score_length = 0
 
         self.init_motion = False
 
